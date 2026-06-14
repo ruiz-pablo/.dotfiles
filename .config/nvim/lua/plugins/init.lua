1a6a3efb591b3cb84b73c363d402a622e44b1a3a
Pablo Ruiz <pablo785@proton.me>
2026-06-14 18:46:37 +0200

    Load files and symbolic links

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 local plugins_dir = vim.fs.joinpath(vim.fn.stdpath("config"), "lua", "plugins")
 
 for file_name, type in vim.fs.dir(plugins_dir, { depth = 1, follow = true }) do
-	if type == "file" and file_name ~= "init.lua" then
+	if (type == "file" or type == "link") and file_name ~= "init.lua" then
 		local module = file_name:gsub("%.lua$", "")
 		require("plugins." .. module)
 	end
